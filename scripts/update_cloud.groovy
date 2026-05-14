43a86911f890420dc9e30c4e9b1cc38ad76ec127
Sanjal <sanjal.comcast@gmail.com>
2026-05-14 18:57:51 +0530

    Clean up update_cloud.groovy and improve AMI logic
    Removed unused commented code and updated AMI handling.

--- scripts/update_cloud.groovy
+++ scripts/update_cloud.groovy
@@ -6,11 +6,10 @@ def instance = Jenkins.instance
 def cloud = instance.clouds.getByName("gami2023")
 
 if (cloud == null) {
+
     println("Cloud not found")
     return
 }
-
-def latestAmi = binding.variables['LATEST_AMI']
 
 println("Latest AMI: ${latestAmi}")
 
@@ -26,7 +25,41 @@ cloud.templates.each { template ->
     }
 }
 
-
 instance.save()
 
 println("Jenkins cloud configuration saved")
+
+
+
+// import jenkins.model.*
+// import hudson.plugins.ec2.*
+
+// def instance = Jenkins.instance
+
+// def cloud = instance.clouds.getByName("gami2023")
+
+// if (cloud == null) {
+//     println("Cloud not found")
+//     return
+// }
+
+// def latestAmi = binding.variables['LATEST_AMI']
+
+// println("Latest AMI: ${latestAmi}")
+
+// cloud.templates.each { template ->
+
+//     if (template.description == "goldenami") {
+
+//         println("Updating template AMI...")
+
+//         template.ami = latestAmi
+
+//         println("Updated AMI to: ${latestAmi}")
+//     }
+// }
+
+
+// instance.save()
+
+// println("Jenkins cloud configuration saved")
